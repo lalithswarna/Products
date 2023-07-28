280ee5fe0de44954ac58fa37f207e6cd369b072f
user <user@DESKTOP-D7BOV38>
2023-07-28 15:59:52 -0500

    Add project files.

---
+++ Product.cs
@@ -0,0 +1,11 @@
+namespace Products
+{
+    public class Product
+    {
+        public int ID { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
